c8eeb02020b294d3ee180d1b7c5ed830c70fd931
John Fish <j31@users.noreply.github.com>
2017-08-07 16:10:56 +0200

    dynamic show pages

--- config.rb
+++ config.rb
@@ -20,3 +20,11 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each do |flat|
+  proxy "/flats/#{flat.user}.html", "/flats/show.html", locals: { flat: flat }, ignore: true
+end
+
+# ["papillard", "ssaunier", "monsieurpaillard"].each do |name|
+#   proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+# end
